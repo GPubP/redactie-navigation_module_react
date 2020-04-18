5ab14abfc3905b7f373f2444db0e1b35f1d9e78d
Jeroen Valcke <jeroen.valcke@studiohyperdrive.be>
2020-04-18 20:03:41 +0200

    feat(contentDetail): implement new show prop

--- public/lib/connectors/content.ts
+++ public/lib/connectors/content.ts
@@ -1,8 +1,13 @@
+import { ContentAPI } from '@redactie/content-module';
+import { ExternalCompartmentOptions } from '@redactie/content-module/dist/lib/store/api/externalCompartments';
 import Core from '@redactie/redactie-core';
 
-const contentModuleAPI = Core.modules.getModuleAPI('content-module');
+const contentModuleAPI: ContentAPI = Core.modules.getModuleAPI('content-module') as ContentAPI;
 
-export const registerContentDetailCompartment = (key: string, options: any): any | false =>
+export const registerContentDetailCompartment = (
+	key: string,
+	options: ExternalCompartmentOptions
+): any | false =>
 	contentModuleAPI
 		? contentModuleAPI.registerContentDetailCompartment('navigation', options)
 		: false;
