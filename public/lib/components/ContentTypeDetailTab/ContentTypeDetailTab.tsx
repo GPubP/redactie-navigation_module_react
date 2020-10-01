19670e4b96b09450e54727d27e9469797fef4bdb
Andry Charlier <andry.charlier@studiohyperdrive.be>
2020-10-01 13:31:25 +0200

    feat(ct-detail-tab): add LeavePrompt

--- public/lib/components/ContentTypeDetailTab/ContentTypeDetailTab.tsx
+++ public/lib/components/ContentTypeDetailTab/ContentTypeDetailTab.tsx
@@ -2,7 +2,7 @@ import { Button, RadioGroup, Select } from '@acpaas-ui/react-components';
 import { ActionBar, ActionBarContentSection } from '@acpaas-ui/react-editorial-components';
 import { ExternalTabProps } from '@redactie/content-types-module';
 import { CORE_TRANSLATIONS } from '@redactie/translations-module/public/lib/i18next/translations.const';
-import { useDetectValueChanges } from '@redactie/utils';
+import { LeavePrompt, useDetectValueChanges } from '@redactie/utils';
 import { Field, Formik } from 'formik';
 import React, { FC, useMemo, useState } from 'react';
 
@@ -25,7 +25,7 @@ const ContentTypeDetailTab: FC<ExternalTabProps> = ({
 	);
 	const [t] = useCoreTranslation();
 	const [formValue, setFormValue] = useState<any | null>(null);
-	const [isChanged] = useDetectValueChanges(!isLoading, formValue);
+	const [hasChanges] = useDetectValueChanges(!isLoading, formValue);
 
 	const onFormSubmit = (values: any): void => {
 		onSubmit({ config: values, validationSchema: {} });
@@ -80,8 +80,8 @@ const ContentTypeDetailTab: FC<ExternalTabProps> = ({
 									</Button>
 									<Button
 										iconLeft={isLoading ? 'circle-o-notch fa-spin' : null}
-										disabled={isLoading || !isChanged}
-										onClick={() => submitForm()}
+										disabled={isLoading || !hasChanges}
+										onClick={submitForm}
 										type="success"
 										htmlType="submit"
 									>
@@ -90,6 +90,7 @@ const ContentTypeDetailTab: FC<ExternalTabProps> = ({
 								</div>
 							</ActionBarContentSection>
 						</ActionBar>
+						<LeavePrompt when={hasChanges} onConfirm={submitForm} />
 					</>
 				);
 			}}
