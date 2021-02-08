6ffc33e95a7ef87e79148add96f0eaf1956418fc
Glenn Verschooren <Glenn.Verschooren@digipolis.be>
2021-02-08 16:43:20 +0100

    chore: remove unused messages

--- public/lib/helpers/contentCompartmentHooks/beforeAfterSubmit.const.ts
+++ public/lib/helpers/contentCompartmentHooks/beforeAfterSubmit.const.ts
@@ -3,11 +3,9 @@ export const ERROR_MESSAGES: Readonly<{
 	delete: string;
 	update: string;
 	rollback: string;
-	moveTreeItem: string;
 }> = {
 	create: 'Aanmaken van item in de navigatieboom is mislukt.',
 	delete: 'Verwijderen van het navigatie item is mislukt.',
 	update: 'Wijzigen van het item in de navigatieboom is mislukt.',
 	rollback: 'Terugrollen aanmaak navigatie item is mislukt.',
-	moveTreeItem: 'Verplaatsen van het item naar een andere navigatieboom is mislukt.',
 };
