c1cf0ef815937859d935817ebfcf8426dff34d2e
De Weerdt Maarten <maarten.deweerdt@cronos.be>
2022-02-14 11:54:36 +0100

    feat(red-1866): exclude settings path in leavePrompt

--- public/lib/views/MenuDetailSettings/MenuDetailSettings.const.ts
+++ public/lib/views/MenuDetailSettings/MenuDetailSettings.const.ts
@@ -1,4 +1,5 @@
 import { object, string } from 'yup';
+import { MODULE_PATHS, SITES_ROOT, TENANT_ROOT } from '../../navigation.const';
 
 export const MENU_SETTINGS_VALIDATION_SCHEMA = object().shape({
 	meta: object().shape({
@@ -11,4 +12,6 @@ export const LANG_OPTIONS = [
 	{ key: 'nl', label: 'Nederlands (NL)', value: 'nl' },
 ];
 
-export const SETTINGS_ALLOWED_LEAVE_PATHS = [];
+export const SETTINGS_ALLOWED_LEAVE_PATHS = [
+	`${TENANT_ROOT}/${SITES_ROOT}${MODULE_PATHS.site.detailSettings}`
+];
