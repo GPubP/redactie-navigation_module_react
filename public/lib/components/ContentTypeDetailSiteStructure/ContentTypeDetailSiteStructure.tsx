8dcfd0f882d47b23aeb94a97a68223a41b25333c
De Weerdt Maarten <maarten.deweerdt@cronos.be>
2022-04-19 10:48:57 +0200

    feat(red-2034): page setup

--- public/lib/components/ContentTypeDetailSiteStructure/ContentTypeDetailSiteStructure.tsx
+++ public/lib/components/ContentTypeDetailSiteStructure/ContentTypeDetailSiteStructure.tsx
@@ -1,39 +1,14 @@
 import { RadioGroup } from '@acpaas-ui/react-components';
-import { Cascader } from '@acpaas-ui/react-editorial-components';
-import { ExternalTabProps, ModuleValue } from '@redactie/content-module';
-import { Field, FormikProps, FormikValues, useFormikContext } from 'formik';
-import React, { FC, useMemo } from 'react';
+import { ExternalTabProps } from '@redactie/content-module';
+import { Field, FormikValues, useFormikContext } from 'formik';
+import React, { FC } from 'react';
 
 import translationsConnector from '../../connectors/translations';
-import { getPositionInputValue, getTreeConfig } from '../../helpers';
-import { useTree, useTreeItem } from '../../hooks';
 import { MODULE_TRANSLATIONS } from '../../i18next/translations.const';
 import { SITE_STRUCTURE_POSITION_OPTIONS } from '../../navigation.const';
-import { CascaderOption, NavItem, NavTree } from '../../navigation.types';
-import { TreeDetailItem } from '../../services/trees';
-import { getInitialFormValues } from '../ContentDetailCompartment/contentDetailCompartment.helpers';
-
 const ContentTypeDetailSiteStructure: FC<ExternalTabProps> = () => {
 	const [tModule] = translationsConnector.useModuleTranslation();
-	const { values, setFieldValue } = useFormikContext<FormikValues>();
-	// TODO: fix get navigationtre, based on lang
-	const [loadingTree, tree] = useTree(values.navigationTree);
-	const [loadingTreeItem, treeItem, treeItemError] = useTreeItem(
-		values.navigationTree,
-		values.id
-	);
-
-	const treeConfig = useMemo<{
-		options: CascaderOption[];
-		activeItem: TreeDetailItem | undefined;
-	}>(() => getTreeConfig(tree, values.id), [tree, values.id]);
-
-	const handlePositionOnChange = (
-		value: number[],
-		setFieldValue: FormikProps<FormikValues>['setFieldValue']
-	): void => {
-		setFieldValue('position', value);
-	};
+	const { values } = useFormikContext<FormikValues>();
 
 	return (
 		<div>
@@ -55,32 +30,7 @@ const ContentTypeDetailSiteStructure: FC<ExternalTabProps> = () => {
 			</div>
 			<div className="row u-margin-top">
 				<div className="col-xs-12">
-					{!loadingTree && (
-						<div className="a-input has-icon-right">
-							<label className="a-input__label" htmlFor="text-field">
-								Standaard positie
-							</label>
-							<Cascader
-								changeOnSelect
-								value={values.position}
-								options={treeConfig.options}
-								onChange={(value: number[]) =>
-									handlePositionOnChange(value, setFieldValue)
-								}
-							>
-								<div className="a-input__wrapper">
-									<input
-										onChange={() => null}
-										placeholder="Kies een positie in de boom"
-										value={getPositionInputValue(
-											treeConfig.options,
-											values.position
-										)}
-									/>
-								</div>
-							</Cascader>
-						</div>
-					)}
+					placeholder standaard positie {values.structurePosition}
 				</div>
 			</div>
 		</div>
