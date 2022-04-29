979c02241d701a22973c93fdb9384f4631a54905
Lotje <lotte-moors@hotmail.com>
2022-04-29 13:41:45 +0200

    fix: use url if no activeLanguage present

--- public/lib/helpers/getSiteUrl.ts
+++ public/lib/helpers/getSiteUrl.ts
@@ -1,6 +1,6 @@
 import { SiteDetailModel } from '@redactie/sites-module';
 
-export const getLangSiteUrl = (site: SiteDetailModel | undefined, language = ''): string =>
+export const getLangSiteUrl = (site: SiteDetailModel | undefined, language = ''): any =>
 	(site?.data.url as any)?.multilanguage || (site?.data.url as any)?.multiLanguage
 		? ((site?.data.url as any) || {})[language] || ''
 		: site?.data.url || '';
